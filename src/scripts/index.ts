a66de2aebe5761423d31f255c9bd06ed4e05432f
renaco <renanromero@gmail.com>
2018-09-17 18:50:50 -0500

    wip site: validation

--- src/scripts/index.ts
+++ src/scripts/index.ts
@@ -1,15 +1,21 @@
-import './../styles/main.scss';
+// import './../styles/main.scss';
 import * as CONST from './const';
+import { validateThisForm } from './validate'
 
-console.log('from typescript,', this)
+// console.log('from typescript,', this)
 
 let myUrl: string = '?token=7ce167de82226';
 let domain: string = '//elrena.co';
-let showEnv:any = <HTMLSpanElement>document.querySelector('#showEnv');
-let myLink: string = domain + myUrl;
+// let showEnv:any = <HTMLSpanElement>document.querySelector('#showEnv');
+// let myLink: string = domain + myUrl;
 
 (() => {
-  showEnv.innerText = myLink;
+  validateForm()
+  // showEnv.innerText = myLink;
 })()
 
-console.log(' is the begining ' + domain + myUrl, CONST.LOCAL_ENV);
+function validateForm() {
+  console.log('send this form');
+}
+
+// console.log(' is the begining ' + domain + myUrl, CONST.LOCAL_ENV);
---
+++ src/scripts/validate.ts
@@ -0,0 +1,4 @@
+
+export function validateThisForm() {
+  console.log('here is validation', this);
+}
