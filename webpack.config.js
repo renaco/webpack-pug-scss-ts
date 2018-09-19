dfd44b3ea9d526242e4b7caf5fb17e0f694c4c68
renaco <renanromero@gmail.com>
2018-09-19 00:21:46 -0500

    wip site: continue to integrate

--- webpack.config.js
+++ webpack.config.js
@@ -1,135 +1,103 @@
 const webpack = require('webpack');
 const path = require('path');
-
-/*
- * We've enabled UglifyJSPlugin for you! This minifies your app
- * in order to load faster and run less javascript.
- *
- * https://github.com/webpack-contrib/uglifyjs-webpack-plugin
- *
- */
-
-const UglifyJSPlugin = require('uglifyjs-webpack-plugin');
-
-const { CheckerPlugin } = require('awesome-typescript-loader');
 const CircularDependencyPlugin = require('circular-dependency-plugin');
+const UglifyJSPlugin = require('uglifyjs-webpack-plugin');
+const { CheckerPlugin } = require('awesome-typescript-loader');
 
-/*
- * SplitChunksPlugin is enabled by default and replaced
- * deprecated CommonsChunkPlugin. It automatically identifies modules which
- * should be splitted of chunk by heuristics using module duplication count and
- * module category (i. e. node_modules). And splits the chunks…
- *
- * It is safe to remove "splitChunks" from the generated configuration
- * and was added as an educational example.
- *
- * https://webpack.js.org/plugins/split-chunks-plugin/
- *
- */
-
-const PATHS = {
-	src: path.join(__dirname, 'src'),
-	dist: path.join(__dirname, 'dist')
-};
-
-/*
-*
-* Inline Plugin
-*
-*/
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const HtmlWebpackInlineSourcePlugin = require('html-webpack-inline-source-plugin');
-/*
-//
-// Clean Dist
-//
-*/
-
 const CleanPlugin = require('clean-webpack-plugin');
 
+const PATHS = {
+  entries: path.join(__dirname, 'src/scripts'),
+  src: path.join(__dirname, 'src'),
+  dist: path.join(__dirname, 'dist')
+};
+
+function compileTemplate(name) {
+  return new HtmlWebpackPlugin({
+    filename: name + '.html',
+    template: PATHS.src + '/views/' + name + '.pug',
+    inject: false
+  })
+}
+
 module.exports = {
-	entry:
-		PATHS.src + '/scripts/index.ts'
-	,
-
-	output: {
-		path: PATHS.dist
-	},
-
-	module: {
-		rules: [
-			{
-				test: /\.pug$/,
-				use: [
-					'pug-loader'
-				]
-			},
-
-			{
-				test: /\.scss|css$/,
-				use: [
-					'style-loader',
-					'css-loader',
-					'sass-loader'
-				]
-			},
-
-			{
-				enforce: 'pre',
-				test: /\.tsx?$/,
-				exclude: /node_modules/,
-				loader: 'eslint-loader'
-			},
-
-			{
-				test: /\.tsx?$/,
-				loader: 'awesome-typescript-loader',
-				exclude: /(node_modules)/
-			}
-
-		]
-	},
-
-	resolve: {
-		extensions: ['.ts', '.js']
-	},
-
-	plugins: [
-		new CleanPlugin(PATHS.dist),
-		new UglifyJSPlugin(),
-		new CheckerPlugin(),
-		new CircularDependencyPlugin({
-			exclude: /a\.ts|node_modules/,
-			failOnError: true,
-			cwd: process.cwd()
-		}),
-		new HtmlWebpackPlugin({
-			template: PATHS.src + '/views/index.pug',
-			inlineSource: '.(js|css)'
-		}),
-		new HtmlWebpackInlineSourcePlugin()
-	],
-
-	mode: 'production',
-
-	devServer: {
-		contentBase: PATHS.dist,
-		watchContentBase: true
-	},
-
-	optimization: {
-		splitChunks: {
-			chunks: 'async',
-			minSize: 30000,
-			minChunks: 1,
-			name: false,
-
-			cacheGroups: {
-				vendors: {
-					test: /[\\/]node_modules[\\/]/,
-					priority: -10
-				}
-			}
-		}
-	}
+  entry: {
+    index: PATHS.entries + '/index.ts',
+    notFound: PATHS.entries + '/notFound.ts'
+  },
+  output: {
+    path: PATHS.dist,
+    filename: 'js/[name].js'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.pug$/,
+        use: [
+          'pug-loader'
+        ]
+      },
+      {
+        test: /\.scss|css$/,
+        use: [
+          'css-loader',
+          'sass-loader',
+          'style-loader'
+        ]
+      },
+      {
+        enforce: 'pre',
+        test: /\.tsx?$/,
+        exclude: /node_modules/,
+        loader: 'eslint-loader'
+      },
+      {
+        test: /\.tsx?$/,
+        loader: 'awesome-typescript-loader',
+        exclude: /(node_modules)/
+      }
+    ]
+  },
+  resolve: {
+    extensions: ['.ts', '.js']
+  },
+  plugins: [
+    compileTemplate('index'),
+    compileTemplate('404'),
+    // new ExtractTextPlugin({
+    //   filename: PATHS.dist + 'css/[name].css',
+    //   allChunks: true
+    // }),
+    // new CleanPlugin(PATHS.dist),
+    new UglifyJSPlugin(),
+    new CheckerPlugin(),
+    new CircularDependencyPlugin({
+      exclude: /a\.ts|node_modules/,
+      failOnError: true,
+      cwd: process.cwd()
+    }),
+    new HtmlWebpackInlineSourcePlugin()
+  ],
+  mode: 'production',
+  devServer: {
+    contentBase: PATHS.dist,
+    watchContentBase: true
+  },
+  optimization: {
+    splitChunks: {
+      chunks: 'async',
+      minSize: 30000,
+      minChunks: 1,
+      name: false,
+      cacheGroups: {
+        vendors: {
+          test: /[\\/]node_modules[\\/]/,
+          priority: -10
+        }
+      }
+    }
+  }
 };
