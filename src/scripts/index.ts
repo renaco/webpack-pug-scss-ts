e3bf75a22adaa54def7a89538373e8fe3be1d1f1
kevanz10 <krojas1171@gmail.com>
2020-02-14 11:23:22 -0500

    make it better

--- src/scripts/index.ts
+++ src/scripts/index.ts
@@ -1,8 +1,7 @@
 import './../styles/main.scss';
 import * as CONST from './const';
-
-let myUrl: string = '?token=7ce167de82226';
-let domain: string = `yourDomain.co`;
+let myUrl: string = '?token_key=mJ6psqPVh52WkuUwiTKoGwtt';
+let domain: string = `dev.golazzos.com/`;
 let showEnv: any = <HTMLSpanElement>document.querySelector('#showEnv');
 let myLink: string = ` www.${domain}/${myUrl}`;
 
