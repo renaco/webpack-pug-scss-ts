efe1638420eb6420ba03c2fa593c984527a6d111
renaco <renanromero@gmail.com>
2020-02-21 02:48:58 -0500

    wip begin to funcionality

--- src/scripts/const.ts
+++ src/scripts/const.ts
@@ -1,1 +1,2 @@
 export const LOCAL_ENV = 'localhost';
+export const getHeight = window.innerHeight;
--- src/scripts/index.ts
+++ src/scripts/index.ts
@@ -4,5 +4,7 @@ let myUrl: string = '?token_key=mJ6psqPVh52WkuUwiTKoGwtt';
 let domain: string = `dev.golazzos.com`;
 let showEnv: any = <HTMLSpanElement>document.querySelector('#showEnv');
 let myLink: string = ` www.${domain}/${myUrl}`;
+const myIframe  = <HTMLIFrameElement>document.getElementById('iframeContainer');
+const toUpdateHeightIframe = CONST.getHeight;
 
 console.log('Is the beginning ');
